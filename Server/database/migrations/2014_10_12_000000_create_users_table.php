42bffa81069d6c289f795d07a1a5acf258b91217
SeiryuJP <seiryujp7@gmail.com>
2022-11-21 18:52:53 +0100

    campo foto

--- Server/database/migrations/2014_10_12_000000_create_users_table.php
+++ Server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->string('stats');
+            $table->string('avatar');
             $table->timestamps();
         });
     }
