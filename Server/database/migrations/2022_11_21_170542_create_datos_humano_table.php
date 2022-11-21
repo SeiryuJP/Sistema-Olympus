9ec87703e26f91335649c61fea8a23820897ae14
SeiryuJP <seiryujp7@gmail.com>
2022-11-21 18:11:57 +0100

    datos humano

---
+++ Server/database/migrations/2022_11_21_170542_create_datos_humano_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('datos_humano', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('destino');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('datos_humano');
+    }
+};
