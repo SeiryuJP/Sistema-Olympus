f6f1abb3d2483f3093436a6e7d1c4fe5c9368354
MiriamOliver <miriamoliver99@gmail.com>
2022-12-06 23:49:05 +0100

    cambios migration prueba y obtencion de datos

--- Client/src/js/pruebas.js
+++ Client/src/js/pruebas.js
@@ -1,25 +1,41 @@
 import {obtenerListaPruebas} from './crud_pruebas';
 import { borrarPrueba } from './crud_pruebas';
+import { Prueba } from './classes/Prueba';
+import {listPruebas} from './indexListado';
 
-const lista = document.querySelector('.contenedor-tabla');
+const divLista = document.querySelector('.contenedor-tabla');
 const edit = document.querySelectorAll('.edit');
 
 export const init = async() => {
     const pruebas = await obtenerListaPruebas();
-    pruebas.forEach( crearFilaPrueba );
+    conseguirPruebas(pruebas);
     eliminarPrueba();
+}
+
+const conseguirPruebas = (pruebas) =>{
+    pruebas.forEach(elemento =>{
+        elemento.forEach(crearPrueba);
+    })
+}
+
+const crearPrueba = (prueba) => {
+    console.log(prueba);
+    let newPrueba = new Prueba(prueba.id, prueba.tipo, prueba.iddios, prueba.pregunta, prueba.destino, prueba.updated_at);
+    listPruebas.nuevaPrueba(newPrueba);
+    crearFilaPrueba(newPrueba);
 }
 
 const crearFilaPrueba = ( prueba ) => {
     const fila = (dios, fecha) => `
-    <div class="card mb-3" id="p${prueba.id}">
+    <div class="card mb-3 ps-4 pe-1" id="p${prueba.id}">
         <div class="card-body">
             <div class="row">
-                <div class="col-2">${prueba.id}</div>
-                <div class="col-2">${dios}</div>
-                <div class="col-2">${prueba.destino}</div>
-                <div class="col-4">${fecha.replace('T', ' | ')}</div>
-                <div class="col-2 acciones">
+                <div class="col-2">${prueba.tipo}</div>
+                <div class="col-1">${dios}</div>
+                <div class="col-4">${prueba.pregunta}</div>
+                <div class="col-1">${prueba.destino}</div>
+                <div class="col-3">${fecha.replace('T', ' | ')}</div>
+                <div class="col-1 acciones">
                     <div class="row">
                         <div class="col-6"><i class="fa fa-pencil-square edit" aria-hidden="true"></i></div>
                         <div class="col-6"><i class="fa fa-trash delete" aria-hidden="true"></i></div>
@@ -29,12 +45,11 @@ const crearFilaPrueba = ( prueba ) => {
         </div>
     </div>
     `;
-
     let dios = nombreDios(prueba.iddios);
-    let fecha = prueba.updated_at.slice(0, -8);
+    let fecha = prueba.fecha.slice(0, -8);
     const div = document.createElement('div');
     div.innerHTML = fila (dios, fecha);
-    lista.appendChild(div);
+    divLista.appendChild(div);
 }
 
 const nombreDios = (id) => {
@@ -59,6 +74,7 @@ export const eliminarPrueba = () => {
         boton.addEventListener('click', () => {
             let origen = boton.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
             let id = origen.id.slice(1);
+            listPruebas.eliminarPrueba(id);
             origen.remove();
             borrarPrueba(id);
         });
